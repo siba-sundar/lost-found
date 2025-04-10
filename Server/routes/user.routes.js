9b222875d65626162e9ae08535ff4b63249e8b2a
Siba Sundar <siba.edu2003@gmail.com>
2025-04-10 23:55:32 +0530

    user routes update

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { editDetails, deleteUser, getUserDetails } from "../controllers/user.controller";
-import { authenticateToken } from "../middlewares/tokenAuth";
+import { editDetails, deleteUser, getUserDetails } from "../controllers/user.controller.js";
+import { authenticateToken } from "../middlewares/tokenAuth.js";
 
 
-const userRoute = express.Routes();
+const userRoute = express.Router();
 
 userRoute.get('/profile', authenticateToken, getUserDetails)
 userRoute.put('/edit', authenticateToken, editDetails);
